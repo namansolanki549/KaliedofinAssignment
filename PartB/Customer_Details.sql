7d5de0196aaac9bfd7737e5f4fb1bce196dd0f79
Naman Solanki <namansolanki549@gmail.com>
2021-01-18 20:13:44 +0530

    Update Customer_Details.sql

--- PartB/Customer_Details.sql
+++ PartB/Customer_Details.sql
@@ -1,2 +1,3 @@
-create table Customer_Details(Customer_ID int auto_increment primary key, Customer_Name varchar(100), Customer_PAN varchar(100), Banned boolean, Customer_Join_Time date, Gender enum('male','female')
+create table Customer_Details(
+Customer_ID int auto_increment primary key, Customer_Name varchar(100), Customer_PAN varchar(100), Banned boolean, Customer_Join_Time date, Gender enum('male','female')
 );
